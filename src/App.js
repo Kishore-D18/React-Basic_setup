a6287b298e5f8c44205386bccc76e034586c0f59
Kishore-D18 <kish.vk86@gmail.com>
2024-02-19 19:58:12 +0530

    Intial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+    console.log("hello");
+    return (
+        
+        
+            <h1>Welcome</h1>
+        
+    )
+}
+
+export default App;
